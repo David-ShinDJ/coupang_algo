1a94ef554aa5adf414679be62e176751f240476c
DavidShin <dnrur124@gmail.com>
2023-10-01 17:01:27 +0900

    datastructure understanding

--- main.py
+++ main.py
@@ -1,25 +1,79 @@
-from my_module import Item, OrderList
-## 객체지향 코드예시
-class User:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+from my_module import Item, Shelf, PickList
+import uuid, random
+
+def get_random_shelf(shelf_list):
+    if not shelf_list:
+        return None
+    random_index = random.randint(0, len(shelf_list) - 1)
+    return shelf_list[random_index]
+
+# Shelf 객체 생성
+shelf_list = []
+shelf_a = Shelf("A", 1)
+shelf_list.append(shelf_a)
+shelf_a2 = Shelf("A", 2)
+shelf_list.append(shelf_a2)
+shelf_a3 = Shelf("A", 3)
+shelf_list.append(shelf_a3)
+shelf_a4 = Shelf("A", 4)
+shelf_list.append(shelf_a4)
+shelf_a5 = Shelf("A", 5)
+shelf_list.append(shelf_a5)
+shelf_b = Shelf("B", 1)
+shelf_list.append(shelf_b)
+shelf_b2 = Shelf("B", 2)
+shelf_list.append(shelf_b2)
+shelf_b3 = Shelf("B", 3)
+shelf_list.append(shelf_b3)
+shelf_b4 = Shelf("B", 4)
+shelf_list.append(shelf_b4)
+shelf_b5 = Shelf("B", 5)
+shelf_list.append(shelf_b5)
+shelf_c = Shelf("C", 1)
+shelf_list.append(shelf_c)
+shelf_c2 = Shelf("C", 2)
+shelf_list.append(shelf_c2)
+shelf_c3 = Shelf("C", 3)
+shelf_list.append(shelf_c3)
+shelf_c4 = Shelf("C", 4)
+shelf_list.append(shelf_c4)
+shelf_c5 = Shelf("C", 5)
+shelf_list.append(shelf_c5)
+
+random_shelf = get_random_shelf(shelf_list)
+# Item 객체 생성
+
+item_1 = Item(uuid.uuid4(), "컴퓨터 마우스", 20000, get_random_shelf(shelf_list))
+item_2 = Item(uuid.uuid4(), "노트북 가방", 35000, get_random_shelf(shelf_list))
+item_3 = Item(uuid.uuid4(), "무선 키보드", 45000, get_random_shelf(shelf_list))
+item_4 = Item(uuid.uuid4(), "스마트폰 케이스", 15000, get_random_shelf(shelf_list))
+item_5 = Item(uuid.uuid4(), "무선 이어폰", 65000, get_random_shelf(shelf_list))
+item_6 = Item(uuid.uuid4(), "헤드폰", 50000, get_random_shelf(shelf_list))
+item_7 = Item(uuid.uuid4(), "세탁기", 550000, get_random_shelf(shelf_list))
+item_8 = Item(uuid.uuid4(), "냉장고", 700000, get_random_shelf(shelf_list))
+item_9 = Item(uuid.uuid4(), "전자레인지", 45000, get_random_shelf(shelf_list))
+item_10 = Item(uuid.uuid4(), "청소기", 90000, get_random_shelf(shelf_list))
+item_11 = Item(uuid.uuid4(), "디지털 카메라", 1200000, get_random_shelf(shelf_list))
+item_12 = Item(uuid.uuid4(), "스피커", 55000, get_random_shelf(shelf_list))
+item_13 = Item(uuid.uuid4(), "음식 조리기", 80000, get_random_shelf(shelf_list))
+item_14 = Item(uuid.uuid4(), "에어컨", 900000, get_random_shelf(shelf_list))
+item_15 = Item(uuid.uuid4(), "테이블 램프 ", 25000, get_random_shelf(shelf_list))
+item_16 = Item(uuid.uuid4(), "서랍장", 130000, get_random_shelf(shelf_list))
+item_17 = Item(uuid.uuid4(), "식탁 의자", 45000, get_random_shelf(shelf_list))
+item_18 = Item(uuid.uuid4(), "소파", 350000, get_random_shelf(shelf_list))
+item_19 = Item(uuid.uuid4(), "침대 시트", 60000, get_random_shelf(shelf_list))
+item_20 = Item(uuid.uuid4(), "수영복", 35000, get_random_shelf(shelf_list))
+item_21 = Item(uuid.uuid4(), "운동화", 80000, get_random_shelf(shelf_list))
+item_22 = Item(uuid.uuid4(), "스니커즈", 70000, get_random_shelf(shelf_list))
+item_23 = Item(uuid.uuid4(), "셔츠", 35000, get_random_shelf(shelf_list))
+item_24 = Item(uuid.uuid4(), "원피스", 55000, get_random_shelf(shelf_list))
+item_25 = Item(uuid.uuid4(), "청바지", 45000, get_random_shelf(shelf_list))
+item_26 = Item(uuid.uuid4(), "모자", 15000, get_random_shelf(shelf_list))
+item_27 = Item(uuid.uuid4(), "선글라스", 20000, get_random_shelf(shelf_list))
+item_28 = Item(uuid.uuid4(), "화장품 세트", 75000, get_random_shelf(shelf_list))
+item_29 = Item(uuid.uuid4(), "휴대용 충전기", 30000, get_random_shelf(shelf_list))
 
 
-    def greeting(self):
-        print(f"Hi My Name is {self.name}, and I am {self.age} years old")
+print(item_26.location.sector)
 
-
-# 테스트
-David = User("David", 29)
-July = User("July", 25)
-
-David.greeting()
-July.greeting()
-
-First_Item = Item(1, "ADD", 1)
-
-First_Order = OrderList([First_Item])
-
-First_Order.read()
-
+print(shelf_a3.items)
--- my_module.py
+++ my_module.py
@@ -1,32 +1,34 @@
-class Item:
-    def __init__(self, id, location, priority):
-        self.id = id
-        self.location = location
-        self.priority = priority
+import uuid  # uuid 모듈을 추가해야 합니다.
 
-    def move_location(self, newLocation):
-        knewLocation = self.location
-        self.location = newLocation
-        print(f"Location {knewLocation} Changed to {self.location}")
+class Shelf:
+    def __init__(self, sector, number):
+        self.sector = sector
+        self.number = number
+        self.items = []
 
-    def update_priority(self, newPriority):
-        print("Hello")
-
-
-
-class OrderList:
-    def __init__(self, items):
-        self.items = items
-
-    def add(self, item):
+    def add_item(self, item):
         self.items.append(item)
 
-    def delete(self, item):
-        print(f"The Item: {item} is Deleted")
+class Item:
+    def __init__(self, id, name, price, shelf):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.location = shelf  # shelf.sector + str(shelf.number) 대신 shelf 객체를 직접 할당
+
+class PickList:
+    def __init__(self):
+        self.items = []
+
+    def pick_item(self, item):
+        self.items.append(item)
+        print(f"The Item: {item.name} is Picked")  # item을 출력할 때 item.name을 사용
+        item.location.items.remove(item)
+    def put_item(self, item):
         self.items.remove(item)
+        item.location.items.append(item)
+        print(f"The Item: {item.name} is put on Shelf")  # item을 출력할 때 item.name을 사용
 
     def read(self):
         for item in self.items:
-            print(f"{item.location}")
-
-
+            print(f"{item.name}")
